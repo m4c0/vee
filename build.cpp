016b0bedacab46f52e475510388ae902bcc4afd8
Eduardo Costa <m4c0@users.noreply.github.com>
2023-01-29 12:30:28 +0000

    blocking build on wasm
    since there's no support for Vulkan

--- build.cpp
+++ build.cpp
@@ -7,5 +7,14 @@ int main(int argc, char **argv) {
   m->add_include_dir("vulkan-headers/include");
   m->add_part("vulkan");
 
-  return run_main(m, argc, argv);
+  auto poc = unit::create<app>("vee-poc");
+  poc->add_ref(m);
+  poc->add_unit<>("poc");
+
+  auto pf = unit::create<per_feat<seq>>("vee-filter");
+  pf->for_feature(android_ndk).add_ref(poc);
+  pf->for_feature(objective_c).add_ref(poc);
+  pf->for_feature(windows_api).add_ref(poc);
+
+  return run_main(pf, argc, argv);
 }
---
+++ poc.cpp
@@ -0,0 +1,3 @@
+import vee;
+
+int main() {}
