2a243145f6161aee4c73bf188b249287219f311c
Eduardo Costa <m4c0@users.noreply.github.com>
2026-02-19 22:13:27 +0000

    bitonic: temp stuff

--- poc-bitonic.cpp
+++ poc-bitonic.cpp
@@ -6,6 +6,11 @@ import silog;
 import sires;
 import vee;
 
+struct upc {
+  unsigned jump;
+  unsigned div;
+};
+
 int main() try {
   auto i = vee::create_instance("vee-poc-bitonic");
   auto dbg = vee::create_debug_utils_messenger();
@@ -20,12 +25,12 @@ int main() try {
     vee::dsl_compute_storage(),
     vee::dsl_compute_storage(),
   });
-  auto pl = vee::create_pipeline_layout(*dsl);
+  auto pl = vee::create_pipeline_layout(*dsl, vee::compute_push_constant_range<upc>());
 
   auto dpool = vee::create_descriptor_pool(2, { vee::storage_buffer(4) });
 
-  auto ds0 = vee::allocate_descriptor_set(*dpool, *dsl);
-  auto ds1 = vee::allocate_descriptor_set(*dpool, *dsl);
+  auto ds01 = vee::allocate_descriptor_set(*dpool, *dsl);
+  auto ds10 = vee::allocate_descriptor_set(*dpool, *dsl);
 
   auto mem0 = vee::create_host_memory(pd, buf_sz);
   auto buf0 = vee::create_buffer(buf_sz, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
@@ -36,10 +41,10 @@ int main() try {
   vee::bind_buffer_memory(*buf1, *mem1, 0);
 
   // We "ping-pong" between bitonic stages
-  vee::update_descriptor_set(ds0, 0, *buf0);
-  vee::update_descriptor_set(ds0, 1, *buf1);
-  vee::update_descriptor_set(ds1, 0, *buf1);
-  vee::update_descriptor_set(ds1, 1, *buf0);
+  vee::update_descriptor_set(ds01, 0, *buf0);
+  vee::update_descriptor_set(ds01, 1, *buf1);
+  vee::update_descriptor_set(ds10, 0, *buf1);
+  vee::update_descriptor_set(ds10, 1, *buf0);
 
   auto kern = vee::create_shader_module(sires::slurp("poc-bitonic.comp.spv"));
   auto p = vee::create_compute_pipeline(*pl, *kern, "main");
@@ -50,23 +55,42 @@ int main() try {
 
   {
     auto p = static_cast<unsigned *>(vee::map_memory(*mem0));
-    for (auto i = 0; i < elems; i++) p[i] = i;
-    for (auto i = 0; i < elems; i++) {
-      for (auto j = i + 1; j < elems; j++) {
-        if (rng::randf() >= 0.5) continue;
-        auto tmp = p[i];
-        p[i] = p[j];
-        p[j] = tmp;
-      }
-    }
+    const unsigned article_data[] {
+      2, 8, 5, 6, 10, 15, 1, 4, 11, 13, 12, 3, 17, 9, 7, 21
+    };
+    for (auto i = 0; i < 16; i++) p[i] = article_data[i];
+    //for (auto i = 0; i < elems; i++) p[i] = i;
+    //for (auto i = 0; i < elems; i++) {
+    //  for (auto j = i + 1; j < elems; j++) {
+    //    if (rng::randf() >= 0.5) continue;
+    //    auto tmp = p[i];
+    //    p[i] = p[j];
+    //    p[j] = tmp;
+    //  }
+    //}
     vee::unmap_memory(*mem0);
   }
 
+  upc pc {};
   {
     vee::begin_cmd_buf_one_time_submit(cb);
     vee::cmd_bind_c_pipeline(cb, *p);
-    vee::cmd_bind_c_descriptor_set(cb, *pl, 0, ds0);
-    vee::cmd_dispatch(cb, elems / 2, 1, 1);
+
+    pc = { .jump = 1, .div = 2 };
+    vee::cmd_bind_c_descriptor_set(cb, *pl, 0, ds01);
+    vee::cmd_push_compute_constants(cb, *pl, &pc);
+    vee::cmd_dispatch(cb, elems, 1, 1);
+
+    //pc = { .jump = 2, .div = 4 };
+    //vee::cmd_bind_c_descriptor_set(cb, *pl, 0, ds10);
+    //vee::cmd_push_compute_constants(cb, *pl, &pc);
+    //vee::cmd_dispatch(cb, elems / 4, 1, 1);
+
+    //pc = { .jump = 1, .div = 4 };
+    //vee::cmd_bind_c_descriptor_set(cb, *pl, 0, ds01);
+    //vee::cmd_push_compute_constants(cb, *pl, &pc);
+    //vee::cmd_dispatch(cb, elems / 4, 1, 1);
+
     vee::end_cmd_buf(cb);
   }
   vee::queue_submit({
@@ -76,10 +100,19 @@ int main() try {
   });
   vee::device_wait_idle();
 
+  silog::info("Memory 0");
+  {
+    auto p = static_cast<unsigned *>(vee::map_memory(*mem0));
+    for (auto i = 0; i < elems; i += 4) {
+      silog::log(silog::info, ">>>>> %d %d %d %d", p[i], p[i+1], p[i+2], p[i+3]);
+    }
+    vee::unmap_memory(*mem0);
+  }
+  silog::info("Memory 1");
   {
     auto p = static_cast<unsigned *>(vee::map_memory(*mem1));
-    for (auto i = 0; i < elems; i++) {
-      silog::log(silog::info, ">>>>> %d", p[i]);
+    for (auto i = 0; i < elems; i += 4) {
+      silog::log(silog::info, ">>>>> %d %d %d %d", p[i], p[i+1], p[i+2], p[i+3]);
     }
     vee::unmap_memory(*mem1);
   }
